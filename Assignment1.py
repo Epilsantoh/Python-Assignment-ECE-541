7b493734062c46600a251d58e55ef90cd300a2b6
Epilsantoh <117914496+Epilsantoh@users.noreply.github.com>
2022-11-10 13:35:42 +0100

    Assignment 1

---
+++ Assignment1.py
@@ -0,0 +1,14 @@
+#Name: Amadi Uchechukwu Francis 
+#Reg No. - 2017364027
+#Dept: Electronic And Computer Engineering
+
+value1 = int(input("Enter a number: "))
+value2 = int(input("Enter a number: "))
+sum = value1 + value2
+minus = value1 - value2
+multiple = value1 * value2
+division = value1 / value2
+print(f"The sum of {value1} and {value2} is => {sum}")
+print(f"The difference between {value1} and {value2} is => {minus}")
+print(f"The product of {value1} and {value2} is => {multiple}")
+print(f"The quotient of {value1} and {value2} is => {division}")
