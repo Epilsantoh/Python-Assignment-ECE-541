04115addc56be6f9ae56bfd3b54859a30447b377
Epilsantoh <117914496+Epilsantoh@users.noreply.github.com>
2022-11-10 13:39:01 +0100

    Assignment 3

---
+++ Assignment3.py
@@ -0,0 +1,6 @@
+
+NameOfPerson = input("What is your name?")
+AgeOfPerson = input("Please input your age")
+DeptOfPerson = input("What is the name of your department?")
+
+Print(f" name = {NameOfPerson} /n age = {AgeOfPerson} /n department ={DeptOfPerson}")
